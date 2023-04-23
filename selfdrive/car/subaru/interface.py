bf4b3c0cc35e9c9dcd3ec7fcbce53e17707eb14b
goatstalker <112902583+goatstalker@users.noreply.github.com>
2023-04-23 09:34:21 -0500

    Tuning

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -86,10 +86,10 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 1568. + STD_CARGO_KG
       ret.wheelbase = 2.75
       ret.centerToFront = ret.wheelbase * 0.5
-      ret.steerRatio = 17
+      ret.steerRatio = 17 # learned, 13.5 stock
       ret.steerActuatorDelay = 0.3   # attempt initial values similar to ascent 
       ret.lateralTuning.init('pid')
-      ret.lateralTuning.pid.kf = 0.00003   # attempt initial values similar to ascent 
+      ret.lateralTuning.pid.kf = 0.00004
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]  # attempt initial values similar to ascent 
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.0025, 0.1], [0.00025, 0.01]]  # attempt initial values similar to ascent 
 
